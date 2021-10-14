d592653590c2468d9b411dda1e9659b2827e392f
mads720n <madshjorthhansen@gmail.com>
2021-10-14 12:35:01 +0200

    ved ikke om det gør noget

--- Class_Start.py
+++ Class_Start.py
@@ -1,25 +1,46 @@
 class Start:
 
+    def __init__(self, OnlineMode, addr):
+        self.onlineMode = OnlineMode
+        self.addr = addr
+        print(self.onlineMode)
+
+        self.modeTest = False
+
+        if self.onlineMode == True:
+            print("[Starter WARSHIPGAME i ONLINE-mode]")
+            self.modeTest = True
+        elif self.onlineMode == False:
+            print("[Starter WARSHIPGAME i OFFLINE-mode]")
+            self.modeTest = True
+        else:
+            print('[Error] - onlineMode must be a boolean')
+
+        if self.modeTest == False : print("fejl")
+
     def startServer(self):
         # Tjek om spillet kører online mod spiller eller offline mod en AI.
         # Hvis Online, kør serveren, så den er klar til at forbinde spilerne.
         # Hvis Offline gør AI klar på det niveau der er valgt.
-
-         return True
-
-
+        #placeholder:
+        print(self.onlineMode)
+        online = True
+        return True
+    startServer()
     def forbindSpillere(self):
         # if online = True, forbind begge spillere til serveren.
         # tjek at begge spillere kan modtage og sende data til server.
         # Tjek at begge spillere har loadet spillet korrekt... check()
         # Diceroll omkring, hvem der får lov til at starte.
-
+        print(self.startServer(1))
+        print("snask")
         return True
 
     def loadSpilData(self):
         # Load mappets grid hvor man skal kunne placere skibe på
         # Tjek at spillerne kan connect
         # Load Spillernes data
+
         return True
 
     def aktiverSpil(self):
@@ -38,4 +59,6 @@ class Start:
             print("Alle funktioner er startet korrekt", "\n")
             return True
 
-Start.check(Start)
+Start(True,111)
+
+#Start.check(Start)
